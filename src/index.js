5dd48c19f62cc265deb5ac5dd8012b1348a56d92
Sofia-Zammitto-Benasayag <76660573+Sofia-Zammitto-Benasayag@users.noreply.github.com>
2021-10-17 11:46:18 -0400

    Defined collapsibles

--- src/index.js
+++ src/index.js
@@ -2,4 +2,12 @@ import "./styles.css";
 
 document.getElementById("app").innerHTML = `
 
+
 `;
+
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
